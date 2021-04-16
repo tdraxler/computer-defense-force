f210a6445707864a534111ad6ff98c7170b1e49a
Thomas Draxler <twdrax@gmail.com>
2021-04-15 22:49:05 -0700

    Added empty Player class.

---
+++ src/components/player.js
@@ -0,0 +1,11 @@
+export class Player {
+  // TODO - Make this useful
+
+  construction() {
+    this.score = 0;
+  }
+
+  testFunc() {
+    console.log('test');
+  }
+}
--- src/game.js
+++ src/game.js
@@ -1,5 +1,50 @@
-const testFunc = () => {
-  console.log('To be filled in...');
+import Phaser from 'phaser';
+import { Player } from './components/player';
+
+console.log('Game script loaded successfully!');
+
+let config = {
+  type: Phaser.AUTO,
+  width: 400,
+  height: 300,
+  scale: {
+    mode: Phaser.Scale.FIT
+  },
+  parent: 'game',
+  pixelArt: true,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 100 }
+    }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
 };
 
-testFunc();
+function preload()
+{
+  // TODO
+}
+
+function create()
+{
+  // TODO
+}
+
+function update()
+{
+  // TODO
+}
+
+
+// Begin
+
+// eslint-disable-next-line
+let game = new Phaser.Game(config);
+
+let player = new Player();
+player.testFunc();
