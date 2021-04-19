b07af2dcc6291fb5c536c2e6e8ef551e251554a7
omoito <serratoa186@gmail.com>
2021-04-19 10:12:10 -0700

    solving

--- src/components/virus.js
+++ src/components/virus.js
@@ -5,11 +5,18 @@ export class Virus {
   // TODO - Make this useful
 
   construction() {
-    this.player = 0;
+    //super();
+
+    this.load.spritesheet('enemy1', 'images/virus_v1.png', { frameWidth: 200, frameHeight: 50});
   }
 
   createVirus(){
     //this.virus = this.physics.add.sprite(,,VIRUS_KEY)
+    this.add.sprite(0, 0, 'enemy1');
+  }
+
+  update() {
+
   }
 
   testFunc() {
