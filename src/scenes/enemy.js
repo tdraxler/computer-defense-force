c92c1352efd615a8d7ab1917a8a24e391846099a
omoito <serratoa186@gmail.com>
2021-04-19 12:59:18 -0700

    now placeholder resets

--- src/scenes/enemy.js
+++ src/scenes/enemy.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import { Virus } from '../components/virus';
 
 let enemy;
 let bgm;
@@ -14,29 +15,33 @@ export class FirstEnemy extends Phaser.Scene {
     // Testing setting background sound, 
     // Source:  https://www.fesliyanstudios.com/royalty-free-music/download/a-bit-of-hope/565
     this.load.audio('bgm', ['2020-03-22_-_A_Bit_Of_Hope_-_David_Fesliyan.mp3']);
-    this.load.image('enemy1', 'images/Sprite-0002.png');
+    //this.load.image('enemy1', 'images/Sprite-0002.png');
+    this.virus2 = new Virus(this);
   }
 
   create() {
     bgm = this.sound.add('bgm', { loop: true, volume: 0.25 });
     bgm.play();
-    this.virus = this.add.image(this.game.config.height / 2, this.game.config.width, 'enemy1');
-
+    //this.virus = this.add.image(this.game.config.height / 2, this.game.config.width, 'enemy1');
+    this.virus2.createVirus(this.game.config);
     this.walk();
+    //this.walk();
   }
 
   // from here on out adapted from udemy course examples with very minor changes:
   // https://www.udemy.com/course/making-html5-games-with-phaser-3/
   update() {
-    this.virus.x -= 2;
+    /*this.virus.x -= 2;
     if (this.virus.x < 0) {
       this.virus.x = this.game.config.width;
-    }
+    }*/
+
+    this.virus2.update();
   }
 
   walk() {
     this.tweens.add({
-      targets: this.virus,
+      targets: this.virus2,
       duration: 2000,
       x: 0,
       y: 0,
