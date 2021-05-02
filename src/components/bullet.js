9d087478e417a85ab7a096341b63061aa3fe330c
Kirsten C <cartekir@oregonstate.edu>
2021-05-01 20:00:02 -0700

    Updates before latest pull

--- src/components/bullet.js
+++ src/components/bullet.js
@@ -6,11 +6,18 @@ class Bullet extends Phaser.GameObjects.Sprite {
     //link for bullet sprite
     this.load.image('bullet', '../public/images/bullet_5px.png');
   }
-  fire(x, y, angle, dist) {
-    //load bullet image at position of turret
+  //currently is a placeholder
+  //bullet could accelerate towards X enemy using https://phaser.io/examples/v3/view/physics/arcade/accelerate-to
+  fire(x, y, angle, dist, enemy) {
+    var addBullet = this.physics.add.sprite(x, y, 'bullet');//load bullet image at position of turret
+    this.physics.accelerateToObject(addBullet, enemy, 60, 300, 300);
     //angle towards enemy
     //shoot bullet with X speed for some sort of distance/until hits enemy or wall
     //do damage
+    var attack = this.physics.add.overlap(addBullet, enemy, function (destroyBullet){
+      destroyBullet.body.stop();
+      this.physics.world.removeCollider(attack)
+    }, null, this);
     //disappear
   }
   update(){
