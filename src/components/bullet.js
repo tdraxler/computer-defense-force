8fb3a82edfdc5187fcbf82f3e0d1dd7c36a32fc2
Kirsten C <cartekir@oregonstate.edu>
2021-05-02 17:26:42 -0700

    Now error is "Cannot read property 'add' of undefined" when calling this.physics.add.sprite().

--- src/components/bullet.js
+++ src/components/bullet.js
@@ -3,15 +3,21 @@ import {CONST} from '../constants';
 
 //https://gamedevacademy.org/how-to-make-tower-defense-game-with-phaser-3/
 
-export class Bullet extends Phaser.GameObjects.Sprite {
+export class Bullet extends Phaser.Scene {
   constructor() {
     super({
-      key: CONST.SCENES.ENEMY
+      key:CONST.SCENES.START
     });
+  }
+  preload()
+  {
+    this.load.spritesheet('bullet', './images/bullet_5px.png', {frameWidth: 5, frameHeight: 5});
+    //this.load.image('smBullet', '../images/bullet_5px.png');
+    //, {frameWidth:5, frameHeight: 5}
   }
 
   fire(x, y, angle, dist, enemy) {
-    let addBullet = this.physics.add.sprite(this.x, this.y, 'bullet');//load bullet image at position of turret
+    const addBullet = this.physics.add.sprite(this.x, this.y, 'bullet');//load bullet image at position of turret
     addBullet.setVelocity(100, 100);
     this.physics.add.collider(addBullet, enemy);
     this.physics.accelerateToObject(addBullet, enemy, 60, 100, 100);
@@ -23,10 +29,7 @@ export class Bullet extends Phaser.GameObjects.Sprite {
       this.physics.world.removeCollider(attack)
     }, null, this);
   }
-  preload()
-  {
 
-  }
   create()
   {
 
