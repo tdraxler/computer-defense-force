463e57b6987f8577b1c52b8e2f9e1ce4a675cfe0
Abraham <serratab@oregonstate.edu>
2021-05-20 21:22:20 -0700

    New path

--- src/components/walk.js
+++ src/components/walk.js
@@ -8,14 +8,15 @@ export function walk(enemy) {
     {x: this.game.config.width + 50, y: this.game.config.height + 200}, 
     {x: this.game.config.width + 50, y: this.game.config.height + 130}, 
     {x: this.game.config.width + 190, y: this.game.config.height + 130}, 
-    {x: this.game.config.width + 190, y: this.game.config.height + 10},  
-    {x: this.game.config.width - 30, y: this.game.config.height + 10},
-    {x: this.game.config.width - 50, y: this.game.config.height - 50},
+    {x: this.game.config.width + 190, y: this.game.config.height - 170},  
+    {x: this.game.config.width + 80, y: this.game.config.height - 170},
+    {x: this.game.config.width + 80, y: this.game.config.height + 30},
+    {x: this.game.config.width - 20, y: this.game.config.height + 30},
     {x: this.game.config.width - 90, y: this.game.config.height - 90}
   ];
 
   for (let i = 0; i < positions.length; i++) {
-    if (i === 6) {
+    if (i === 7) {
       this.timeline.add({
         targets: enemy,
         duration: 2000,
